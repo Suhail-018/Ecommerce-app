6366c070eaa4366d6953702a53afadf1ee0efa29
Suhail-018 <suhailyksa@gmail.com>
2025-01-01 16:43:01 +0300

    navBar70% completed

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,7 +2,7 @@ import bannerlg from './images/banner/banner-long.jpg'
 import banner1 from './images/banner/banner1.jpg'
 import banner2 from './images/banner/banner2.jpg'
 import banner3 from './images/banner/banner3.jpg'
-import banner4 from './image/banner/banner4.jpg'
+// import banner4 from './images/banner/banner4.jpg'
 import banner5 from './images/banner/banner5.jpg'
 // best product
 import B11  from './images/best-product/B1-1.png'
@@ -12,16 +12,16 @@ import B22  from './images/best-product/B2-2.png'
 import B31   from './images/best-product/B3-1.png'
 import B32   from './images/best-product/B3-2.png'
 import B41   from './images/best-product/B4-1.png'
-import B42   from './images/best-product/B4-2 .png'
+import B42   from './images/best-product/B4-2.png'
 import B51   from './images/best-product/B5-1.png'
 import B52   from './images/best-product/B5-2.png'
 import B61   from './images/best-product/B6-1.png'
 import B62   from './images/best-product/B6-2.png'
 // blog
-import blog1   from './images/blog/blog1 .jpg'
-import blog2   from './images/blog/blog2 .jpg'
+import blog1   from './images/blog/blog1.jpg'
+import blog2   from './images/blog/blog2.jpg'
 import blog3   from './images/blog/blog3.jpg'
-import blog4   from './images/blog/blog4 .jpg'
+import blog4   from './images/blog/blog4.jpg'
 // deals
 import D11 from './images/Deals/D1-1.png'
 import D12 from './images/Deals/D1-2.png'
@@ -37,6 +37,9 @@ import paypal2  from './images/icon/paypal2.jpg'
 import paypal3  from './images/icon/paypal3.jpg'
 import paypal4  from './images/icon/paypal4.jpg'
 import paypal5  from './images/icon/paypal5.jpg'
+import search_icon  from './images/icon/search.png'
+import  contact_icon from './images/icon/contact.png'
+import cart_icon  from './images/icon/shopping-bag.png'
 // newproduct
 import N11  from './images/new-product/N1-1.png'
 import N12 from './images/new-product/N1-2.png'
@@ -56,24 +59,24 @@ import Ds21 from './images/onsale/D2-1.png'
 import Ds22 from './images/onsale/D2-2.png'
 import Ds31 from './images/onsale/D3-1.png'
 import Ds32 from './images/onsale/D3-2.png'
-import Ds41 from './images/Deals/D4-1.png'
-import Ds42 from './images/Deals/D4-2.png'
-import Ds51 from './images/Deals/D5-1.png'
-import Ds52 from './images/Deals/D5-2.png'
-import Ds61 from './images/Deals/D6-1.png'
-import Ds62 from './images/Deals/D6-2.png'
-import Ds71 from './images/Deals/D7-1.png'
-import Ds72 from './images/Deals/D7-2.png'
-import Ds81 from './images/Deals/D8-1.png'
-import Ds82 from './images/Deals/D8-2.png'
-import Ds91 from './images/Deals/D9-1.png'
-import Ds92 from './images/Deals/D9-2.png'
-import Ds101 from './images/Deals/D10-1.png'
-import Ds102 from './images/Deals/D10-2.png'
-import Ds111 from './images/Deals/D11-1.png'
-import Ds112 from './images/Deals/D11-2.png'
-import Ds121 from './images/Deals/D1-2.png'
-import Ds122 from './images/Deals/D2-1.png'
+import Ds41 from './images/onsale/D4-1.png'
+import Ds42 from './images/onsale/D4-2.png'
+import Ds51 from './images/onsale/D5-1.png'
+import Ds52 from './images/onsale/D5-2.png'
+import Ds61 from './images/onsale/D6-1.png'
+import Ds62 from './images/onsale/D6-2.png'
+import Ds71 from './images/onsale/D7-1.png'
+import Ds72 from './images/onsale/D7-2.png'
+import Ds81 from './images/onsale/D8-1.png'
+import Ds82 from './images/onsale/D8-2.png'
+import Ds91 from './images/onsale/D9-1.png'
+import Ds92 from './images/onsale/D9-2.png'
+import Ds101 from './images/onsale/D10-1.png'
+import Ds102 from './images/onsale/D10-2.png'
+import Ds111 from './images/onsale/D11-1.png'
+import Ds112 from './images/onsale/D11-2.png'
+import Ds121 from './images/onsale/D1-2.png'
+import Ds122 from './images/onsale/D2-1.png'
 // men
 import M11  from './images/products/Men/1-1.png'
 import M12 from './images/products/Men/1-2.png'
@@ -135,7 +138,7 @@ export const assets={
     banner1,
     banner2,
     banner3,
-    banner4,
+    // banner4,
     banner5,
     B11,
     B12,
@@ -244,6 +247,9 @@ export const assets={
     testmn1,
     testmn2,
     testmn3,
+    search_icon,
+    cart_icon,
+    contact_icon,
 
 }
 
