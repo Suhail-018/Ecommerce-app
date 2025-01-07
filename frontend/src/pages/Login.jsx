7b296bfabff16c49ada568b8513f48ee4263e6f5
Suhail-018 <suhailyksa@gmail.com>
2025-01-07 23:12:36 +0300

    loginpagedone

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
                                : <p onClick={() => setCurrentState('Login')} className="cursor-pointer"> Login </p>
                               }
                     </div>
-                    <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+                    <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'} </button>
                                               
                          
                  
