5b24bc7e28cd83cdbd89c1c1e195bda6a3793188
Suhail-018 <suhailyksa@gmail.com>
2025-01-21 01:54:48 +0300

    backend setup dependencies as well as env

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:5174' // or whatever port your frontend is running on
+//   }));
+// app.use(express.json());
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.log(err));
+
+// // Routes will be added here
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/products', require('./routes/products'));
+// app.use('/api/cart', require('./routes/cart'));
+// app.use('/api/orders', require('./routes/orders'));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// new from video
+
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
-
-app.use(cors());
+const PORT = process.env.PORT || 4000;
+// middlewares
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+// api endpoint
 
-// Routes will be added here
-
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send("API WORKING")
+}
+)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
